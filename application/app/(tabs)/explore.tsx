c0e6fd51267d02eca21168b7fe1be029a783955e
kxr <kxrury@gmail.com>
2026-03-02 02:39:57 -0500

    custom alias paths

--- application/app/(tabs)/explore.tsx
+++ application/app/(tabs)/explore.tsx
@@ -1,12 +1,12 @@
 import { Image } from 'expo-image';
 import { Platform, StyleSheet } from 'react-native';
 
-import { Collapsible } from '@/components/ui/collapsible';
-import { ExternalLink } from '@/components/external-link';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { IconSymbol } from '@/components/ui/icon-symbol';
+import { Collapsible } from '@c/ui/collapsible';
+import { ExternalLink } from '@c/external-link';
+import ParallaxScrollView from '@c/parallax-scroll-view';
+import { ThemedText } from '@c/themed-text';
+import { ThemedView } from '@c/themed-view';
+import { IconSymbol } from '@c/ui/icon-symbol';
 import { Fonts } from '@/constants/theme';
 
 export default function TabTwoScreen() {
@@ -58,7 +58,7 @@ export default function TabTwoScreen() {
           different screen densities
         </ThemedText>
         <Image
-          source={require('@/assets/images/react-logo.png')}
+          source={require('@a/images/react-logo.png')}
           style={{ width: 100, height: 100, alignSelf: 'center' }}
         />
         <ExternalLink href="https://reactnative.dev/docs/images">
